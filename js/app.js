04c258b020aaa7701aeacbe1c11eda98ecab39ff
INDEZENDE <i.rios.zendejas@gmail.com>
2018-03-12 13:39:07 -0600

    Agregando títulos y buscador

--- js/app.js
+++ js/app.js
@@ -1,19 +1,46 @@
-const apiload = () => { //funcion expression (hoisting le afecta)
-    fetch(`https://api.mercadolibre.com/sites/MLM/search?category=MLM1132`, {method: 'GET'})//da por default get, si queremos otro añadimos asi {}
-        .then(function(response){
-            //console.log(response); //asi no te lo da en json
-            response.json().then(function(result){
-                console.log(result);
-                
-             //   paintMovies(result.results);
-            })
-        })
-        .catch(function(err){
-            console.log(err);
-        });
-};
-apiload();
+// require('../../js/carousel.js') //Para que  el carrusel funcione
+// $('.carousel').carousel();
+//
 
-require('../../js/carousel.js') //Para que  el carrusel funcione
-$('.carousel').carousel();
+  function search(search) {
+    fetch(`https://api.mercadolibre.com/sites/${siteSelected}/search?q=${search}`)
+    .then(function(response) {
+        response.json().then(
+          function(result){
+            // console.log(result);
+            products(result)
+          // console.log(result);
+          }
+        );
+    });
+  }
 
+
+
+var siteSelected = 'MLM';//Pais=México
+var categorie = '';
+
+// Función de busqueda
+$('#btn-search').on('click', function(event) {//El evento se detona al dar enter
+    search($('#search').val());
+});
+
+
+
+
+const products =result=> {
+
+  result.results.forEach((product, index) => {
+    if(index<9){
+    // console.log(product);//Trae el arreglo
+    // console.log(index);//Trae el índice
+    $name = `name${index}`;//Contiene los ID
+    // console.log($name);
+    $nameProductIndex=`$nameProductIndex${index}`;
+    $nameProductIndex=document.getElementById($name);
+    let nameResult=result.results[index].title;//Contiene cada título
+    $nameProductIndex.innerText=nameResult;
+    }
+      })
+
+}
