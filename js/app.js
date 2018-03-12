919192a52a294ef7fb6a358b646411abac774fe7
G4br13l4 <gabolf93@gmail.com>
2018-03-12 08:55:20 -0600

    request API: categoria y productos

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
+const apiload = () => { //funcion expression (hoisting le afecta)
+    fetch(`https://api.mercadolibre.com/sites/MLM/search?category=MLM1132`, {method: 'GET'})//da por default get, si queremos otro añadimos asi {}
+        .then(function(response){
+            //console.log(response); //asi no te lo da en json
+            response.json().then(function(result){
+                console.log(result);
+                
+             //   paintMovies(result.results);
+            })
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+};
+apiload();
+
 require('../../js/carousel.js') //Para que  el carrusel funcione
 $('.carousel').carousel();
+
