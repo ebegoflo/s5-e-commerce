b41e35fd7f1b418d87e014f6a362ade137a583c2
DanivonneCaSan <daniela.castillo.sn@gmial.com>
2018-03-13 15:04:58 -0600

    Adding homeview

--- js/app.js
+++ js/app.js
@@ -1,4 +1,67 @@
+// Electrodimesticos
+// https://api.mercadolibre.com/categories/MLM1575
+// Deportes
+// https://api.mercadolibre.com/categories/MLM1276
+// Gadgets
+// `https://api.mercadolibre.com/sites/MLM/search?q=gadgets
+// Foto
+// https://api.mercadolibre.com/categories/MLM168281
+//MLM1039"
+// Celulares
+// https://api.mercadolibre.com/categories/MLM1055
+// Juguetes
+// `https://api.mercadolibre.com/sites/MLM/search?q=juguetes
+// Consolas y videojuegos
+// https://api.mercadolibre.com/categories/MLM1144
 
+// Imagenes y links a las secciones
+const request = (section, number) =>{
+  fetch(`https://api.mercadolibre.com/categories/${section}`)
+    .then(function(response) {
+        response.json().then(
+          function(data){
+            let template1 = ``;
+            let cont = ``;
+            console.log(data);
+            document.getElementById(`name${number}`).innerText = data.name;
+            console.log(`url("${data.picture}")`);
+            document.getElementById(`div${number}`).setAttribute('style', `background-image:url(${data.picture})`);
+            // document.getElementById(`img${number}`).setAttribute('src', `${data.picture}`);
+
+            console.log(data.picture);
+            data.children_categories.forEach(function(obj, index){
+              console.log(obj.name);
+              cont = `cont${number}`
+              cont = document.getElementById(cont);
+              template1 +=
+              `<p>${obj.name}</p>`
+              cont.innerHTML = template1;
+            });
+          })
+        });
+    }
+
+//Imágenes
+request("MLM1575",0);
+request("MLM1276",1);
+request("MLM1055",2);
+request("MLM1144",3);
+request("MLM1276",4);
+request("MLM1575",5);
+request("MLM1574",6);
+request("MLM1384",7);
+request("MLM1000",8);
+
+$('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    );
+// Fin código página inicial
+// Funciones de busqueda
 document.getElementById("search-product").addEventListener("submit", enter);
 
 
