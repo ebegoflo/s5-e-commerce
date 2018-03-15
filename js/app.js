e4f07c3b6acc8f7c2f255efe5fc779d8b37b9d52
INDEZENDE <i.rios.zendejas@gmail.com>
2018-03-14 21:28:00 -0600

    Completando suma

--- js/app.js
+++ js/app.js
@@ -224,6 +224,13 @@ const products = result => {
             <a href="#" class="quit-item" data-id="${productData.id}">X</a>
           </td>
        `;
+       let actualPrice=productData.price;
+       actualPrice=actualPrice.substring(1,20);
+       let intPrice=parseInt(actualPrice);
+       totalPrice.push(intPrice);
+      const sum = totalPrice.reduce((total, amount) => total + amount);
+       console.log(sum);
+       paypal(sum);
        cartList.appendChild(row);
        saveProductLocalStorage(productData);
   }
@@ -378,4 +385,46 @@ function paintCategory(categoryClicked){
           }
         );
     });
+}
+// funcion PayPal
+function paypal(suma) {
+  paypal.Button.render({
+
+      env: 'sandbox', // sandbox | production
+
+      // PayPal Client IDs - replace with your own
+      // Create a PayPal app: https://developer.paypal.com/developer/applications/create
+      client: {
+          sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+          production: 'AXogq8X2C5u1kEiDR8P8KHsbQfS3YgiyxFd1Ovvjenv8nD-10pOhb4M9xOc_G6T1Adc3HKsdg5iEw1S9'
+      },
+
+      // Show the buyer a 'Pay Now' button in the checkout flow
+      commit: true,
+
+      // payment() is called when the button is clicked
+      payment: function(data, actions) {
+
+          // Make a call to the REST api to create the payment
+          return actions.payment.create({
+              payment: {
+                  transactions: [
+                      {
+                          amount: { total: `${suma}`, currency: 'USD' }
+                      }
+                  ]
+              }
+          });
+      },
+
+      // onAuthorize() is called when the buyer approves the payment
+      onAuthorize: function(data, actions) {
+
+          // Make a call to the REST api to execute the payment
+          return actions.payment.execute().then(function() {
+              window.alert('Payment Complete!');
+          });
+      }
+
+  }, '#paypal-button-container');
 }
